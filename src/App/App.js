eaf79123b79ce1267782ef2ae923195d793150c8
Emilee Mitchell <emcakes@gmail.com>
2020-01-06 20:58:11 -0600

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,25 @@
 import React from 'react';
 import './App.scss';
 // import NavBar from '../components/NavBar/NavBar.js';
-
+import dogsData from '../helpers/data/dogsData';
+import DogPen from '../components/DogPen/DogPen';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
+  state = {
+    dogs: [],
+  }
+
+  componentDidMount() {
+    const dogs = dogsData.getAllDogs();
+    this.setState({ dogs });
+  }
+
   render() {
     return (
     <div className="App">
       <h1>Doggie Day Care</h1>
+      <DogPen dogs={this.state.dogs} />
       </div>
     );
   }
