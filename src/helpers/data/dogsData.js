0df96840a4baa9f25ed49abad96bb7f0e169fdc0
Emilee Mitchell <emcakes@gmail.com>
2020-01-09 21:51:27 -0600

    Read is done

--- src/helpers/data/dogsData.js
+++ src/helpers/data/dogsData.js
@@ -1,28 +1,50 @@
-const dogs = [
-  {
-    id: 'dog1',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Basset_Hound_600.jpg',
-    name: 'Coco',
-    owner: 'Peter Danger',
-    description: 'Burps minimally. Can read (but cannot understand) Hebrew.',
-  },
-  {
-    id: 'dog2',
-    imageUrl: 'http://1kjkdg1axrkd2g03cnboj761.wpengine.netdna-cdn.com/wp-content/uploads/2017/12/braydon-anderson-105552-e1512684107659.jpg',
-    name: 'Chester',
-    owner: 'Buster Brown',
-    description: 'Does not use excessive acronyms. Expertly quotes and recognizes dialogue from early seasons of The Simpsons.',
-  },
-  {
-    id: 'dog3',
-    imageUrl: 'http://1.bp.blogspot.com/-VjM0CmtN-vU/T7YX-LXa09I/AAAAAAAADA0/Vt1oGWEG0lw/s1600/sheepdog+border+collie+shakes+off+water+funny+picture+photo+pulling+faces+raspberry+tongue.jpg',
-    name: 'George Jr',
-    owner: 'George Sr',
-    description: 'Enjoys fine wine.',
+/* eslint-disable max-len */
+// const dogs = [
+//   {
+//     id: 'dog1',
+//     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Basset_Hound_600.jpg',
+//     name: 'Coco',
+//     owner: 'Peter Danger',
+//     description: 'Burps minimally. Can read (but cannot understand) Hebrew.',
+//   },
+//   {
+//     id: 'dog2',
+//     imageUrl: 'http://1kjkdg1axrkd2g03cnboj761.wpengine.netdna-cdn.com/wp-content/uploads/2017/12/braydon-anderson-105552-e1512684107659.jpg',
+//     name: 'Chester',
+//     owner: 'Buster Brown',
+//     description: 'Does not use excessive acronyms. Expertly quotes and recognizes dialogue from early seasons of The Simpsons.',
+//   },
+//   {
+//     id: 'dog3',
+//     imageUrl: 'http://1.bp.blogspot.com/-VjM0CmtN-vU/T7YX-LXa09I/AAAAAAAADA0/Vt1oGWEG0lw/s1600/sheepdog+border+collie+shakes+off+water+funny+picture+photo+pulling+faces+raspberry+tongue.jpg',
+//     name: 'George Jr',
+//     owner: 'George Sr',
+//     description: 'Enjoys fine wine.',
 
-  },
-];
+//   },
+// ];
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
 
-const getAllDogs = () => dogs;
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllDogs = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/dogs.json`)
+    .then((result) => {
+      const allDogsObj = result.data;
+      const dogs = [];
+      if (allDogsObj != null) {
+        Object.keys(allDogsObj).forEach((dogId) => {
+          const newDog = allDogsObj[dogId];
+          newDog.id = dogId;
+          dogs.push(newDog);
+        });
+      }
+      resolve(dogs);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
 
 export default { getAllDogs };
--- src/helpers/data/employeesData.js
+++ src/helpers/data/employeesData.js
@@ -1,24 +1,46 @@
-const employees = [
-  {
-    id: 'employeee1',
-    firstName: 'Oscar',
-    lastName: 'Grouch',
-    phoneNumber: '555-867-5309',
-  },
-  {
-    id: 'employeee2',
-    firstName: 'Mister',
-    lastName: 'Rogers',
-    phoneNumber: '615-516-6156',
-  },
-  {
-    id: 'employeee3',
-    firstName: 'Cesar',
-    lastName: 'Millan',
-    phoneNumber: '789-654-1233',
-  },
-];
+// const employees = [
+//   {
+//     id: 'employeee1',
+//     firstName: 'Oscar',
+//     lastName: 'Grouch',
+//     phoneNumber: '555-867-5309',
+//   },
+//   {
+//     id: 'employeee2',
+//     firstName: 'Mister',
+//     lastName: 'Rogers',
+//     phoneNumber: '615-516-6156',
+//   },
+//   {
+//     id: 'employeee3',
+//     firstName: 'Cesar',
+//     lastName: 'Millan',
+//     phoneNumber: '789-654-1233',
+//   },
+// ];
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
 
-const getAllEmployees = () => employees;
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllEmployees = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/employees.json`)
+    .then((result) => {
+      const allEmployeesObj = result.data;
+      const employees = [];
+      if (allEmployeesObj != null) {
+        Object.keys(allEmployeesObj).forEach((employeeId) => {
+          const newEmployee = allEmployeesObj[employeeId];
+          newEmployee.id = employeeId;
+          employees.push(newEmployee);
+        });
+      }
+      resolve(employees);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
 
 export default { getAllEmployees };
