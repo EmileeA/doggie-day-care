bdbe7c851fd1771a913906d11cd2daa3d678e587
Emilee Mitchell <emcakes@gmail.com>
2020-01-09 21:32:51 -0600

    NavBar is up

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,42 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './App.scss';
-// import NavBar from '../components/NavBar/NavBar.js';
-import dogsData from '../helpers/data/dogsData';
-import DogPen from '../components/DogPen/DogPen';
-import employeesData from '../helpers/data/employeesData';
-import StaffRoom from '../components/StaffRoom/StaffRoom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+import NavBar from '../components/NavBar/NavBar';
+import firebaseConnection from '../helpers/data/connection';
+import Home from '../components/Home/Home';
+
+firebaseConnection();
 
 class App extends React.Component {
   state = {
-    dogs: [],
-    employees: [],
+    authed: false,
   }
 
   componentDidMount() {
-    const dogs = dogsData.getAllDogs();
-    const employees = employeesData.getAllEmployees();
-    this.setState({ dogs, employees });
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
+    const { authed } = this.state;
     return (
     <div className="App">
-      <h1>Doggie Day Care</h1>
-      <div className="row d-flex justify-content-between">
-      <DogPen dogs={this.state.dogs} />
-      <StaffRoom employees={this.state.employees} />
+      <NavBar authed={authed} />
+      <div className="authedContainer">
+        {
+          (authed) ? (<Home />) : (<img className="homeDog m-2" src="https://cdn.wallpaper.com/main/styles/fp_922x565/s3/legacy/gallery/17056799/G_Doggy_Style.jpg" alt="King Dog" />)
+        }
       </div>
     </div>
     );
